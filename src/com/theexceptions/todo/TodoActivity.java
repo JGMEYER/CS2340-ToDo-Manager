8df6830b5ffdd1a0bc8692a556864266e50d9fcd
Justian Meyer <meyer.justian@gmail.com>
2012-02-16 21:17:10 +0000

    Initial Android Commit.

---
+++ src/com/theexceptions/todo/TodoActivity.java
@@ -0,0 +1,13 @@
+package com.theexceptions.todo;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class TodoActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
