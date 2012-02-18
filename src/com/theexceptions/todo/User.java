6b165b7f8a927c8b63811bdc17ec6bdf3ed95d3c
Justian Meyer <meyer.justian@gmail.com>
2012-02-18 20:54:28 +0000

    Added userID attribute to User.java

--- src/com/theexceptions/todo/User.java
+++ src/com/theexceptions/todo/User.java
@@ -1,17 +1,26 @@
 package com.theexceptions.todo;
 
+/**
+ * A Model to store and pass user information.
+ * @author Justian Meyer
+ */
 public class User {
 	
+	private int userID;
 	private String username;
 	private String password;
 	private String name;
 	private String email;
 	
-	public User(String username, String password, String name, String email) {
+	public User(int userID, String username, String password, String name, String email) {
 		this.username = username;
 		this.password = password;
 		this.name = name;
 		this.email = email;
+	}
+	
+	public int getUserID() {
+		return userID;
 	}
 	
 	public String getUsername() {
@@ -28,6 +37,10 @@ public class User {
 	
 	public String getEmail() {
 		return email;
+	}
+	
+	public void setUserID(int userID) {
+		this.userID = userID;
 	}
 	
 	public void setUsername(String username) {
